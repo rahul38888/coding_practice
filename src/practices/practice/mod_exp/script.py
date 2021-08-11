6078f01c27c1871fdee63fe54f75c84c4341804d
rahul38888 <rahul38888@gmail.com>
2021-08-11 12:21:40 +0530

    Modular Exponentiation for large numbers

---
+++ src/practices/practice/mod_exp/script.py
@@ -0,0 +1,27 @@
+# https://www.geeksforgeeks.org/modular-exponentiation-power-in-modular-arithmetic/
+
+# approach is to use (a*b)%m = (a%m*b%m)%m recursively by deciding n by 2 and solving for that
+
+class Solution:
+    def PowMod(self, x, n, m):
+        if m == 1:
+            return 0
+        if n == 1:
+            return x%m
+        pm = self.PowMod(x, n//2, m)
+        if n%2 == 0:
+            return (pm*pm)%m
+        else:
+            return (((pm*pm)%m)*x)%m
+
+
+if __name__ == '__main__':
+    T=int(input())
+    for i in range(T):
+        x, n , m = input().split()
+        x = int(x)
+        n = int(n)
+        m = int(m)
+        ob = Solution()
+        ans = ob.PowMod(x, n, m)
+        print(ans)
