110844f827e4fe2101a60dc927f058ac23000c8d
rahul38888 <rahul38888@gmail.com>
2021-08-13 08:43:12 +0530

    Number of palindromic strings

---
+++ src/practices/practice/no_of_palindroms/script.py
@@ -0,0 +1,33 @@
+# https://practice.geeksforgeeks.org/problems/number-of-palindromic-strings2706/1#
+
+# For 1 character string = k
+# For 2 character string = k
+# For 3 character string = k(k-1)
+# For 4 character string = k(k-1)
+# For 5 character string = k(k-1)(k-2)
+# For N characters string count = k(k-1)...(k - (N-1)//2)
+
+# start with icc = K and k = 0
+# for in 1,...,N
+# calculate palindromes using above method
+
+class Solution:
+    def palindromicStrings(self, N, K ):
+        i_char_count = 1
+        result = 0
+        for i in range(1,N+1):
+            if i%2!=0:
+                i_char_count *= (K - (i-1)//2)
+        result = (result+i_char_count)%(1000000007)
+        return result
+
+
+if __name__ == '__main__':
+    T=int(input())
+    for i in range(T):
+        n,k = input().split()
+        n,k = int(n), int(k)
+
+        ob = Solution()
+        answer = ob.palindromicStrings(n,k)
+        print(answer)
