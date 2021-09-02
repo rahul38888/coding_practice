41ca0dedf357d2ffeedd03e4b7c1ff5555fbbf21
rahul38888 <rahul38888@gmail.com>
2021-09-02 08:54:50 +0530

    Count of sum of consecutives

---
+++ src/practices/count_sum_of_consecutives/script.py
@@ -0,0 +1,54 @@
+# https://practice.geeksforgeeks.org/problems/count-of-sum-of-consecutives3741/1#
+
+# O(n) : Approach is to start adding values from 1, unto a point when either sum  is N or greater than that
+# if sum == N, increase result_count
+# if sum > N, remove the smallest element
+# if sum < N add next element
+# iterate over these steps until largest number become N
+
+# Another approach is by realising that to that be the case for N
+# N should be equal to a + (a+1) + ..... (a+L) or L+1 consecutive numbers
+# which can be solved to L(L+1)/2 + a(L+1)
+# starting with L = 1, try to func a
+# if a is integer, result_count+=1
+# run this on loop while increasing L, until a < 1
+
+class Solution:
+    def getCount_OrderN(self, N):
+        sm = 1
+        lg = -1
+        result_count = 0
+        csum = 0
+        while lg < N:
+            if csum <= N:
+                if csum == N:
+                    result_count+=1
+                lg+=1
+                csum += lg
+            else:
+                csum -= sm
+                sm+=1
+
+        return result_count
+
+    def getCount(self, N):
+        L = 1
+        a = 1.75
+        result_count = 0
+        while a >= 1:
+            if a == int(a):
+                result_count+=1
+
+            a = (N - L*(L+1)/2)/(L+1)
+            L+=1
+
+        return result_count
+
+
+if __name__ == '__main__':
+    t = int (input ())
+    for _ in range (t):
+        N=int(input())
+
+        ob = Solution()
+        print(ob.getCount(N))
