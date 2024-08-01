3c1d5c4728f300f14a89da9e79e4c228c87ff3a5
rahul38888 <rahul38888@gmail.com>
2024-08-01 08:49:35 +0530

    Heap problem

--- src/practices/practice/kth_largest_elements/script.py
+++ src/practices/practice/kth_largest_elements/script.py
@@ -1,20 +1,33 @@
+import heapq
+
 class Solution:
-    '''
+    """
         Problem: https://www.geeksforgeeks.org/batch/gts-1/track/GTS-ARRAY/problem/k-largest-elements4206
 
         Observations
         ------------
-        Simplest sollution can be to sort the array ysing a performant sorking algorithm and fetch k element largest ones.
+        Simplest solution can be to sort the array using a performant working algorithm and fetch the k largest elements
 
-        Another sollution could be to start sorting the array with binary sort algorithm in decreasing order and stop when sorted part is of size we need.
-        This way we need not ro sort whole array, kust k largest.
+        Solution
+        ---------
+        Start adding each element to a min-heap. If heap size goes above k pop min element
+            Exit when there is no element to put into arr left
 
-        Solution:
-            - Start sorting using  binary sort algorithm in decreasing order.
-            - Keep track of the size of sorted array. If it is equla to k, return the sorted subarray.
-    '''
-    def kLargest(self,arr, n, k):
-	    pass
+        From heap pop all elements one by one into an array. Return that array in reverse order
+    """
+
+    def kLargest(self, arr, n, k):
+        heap = []
+        for a in arr:
+            heapq.heappush(heap, a)
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        result = []
+        while len(heap):
+            result.append(heapq.heappop(heap))
+
+        return result[::-1]
 
 
 if __name__ == '__main__':
