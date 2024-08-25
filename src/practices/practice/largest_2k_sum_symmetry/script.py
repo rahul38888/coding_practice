a494a8a4adcb1d61fe21dd674a67d297bc8005a7
rahul38888 <rahul38888@gmail.com>
2024-08-25 10:03:16 +0530

    Largest consecutive subseq size

--- src/practices/practice/largest_2k_sum_symmetry/script.py
+++ src/practices/practice/largest_2k_sum_symmetry/script.py
@@ -1,15 +1,15 @@
+# Link not found????
 
-
-def largest_sum_symmetry(str):
-    n = len(str)
-    sums = [0]*(n+1)
-    for i in range(1,n+1):
-        sums[i] = sums[i-1] + int(str[i-1])
+def largest_sum_symmetry(string: str):
+    n = len(string)
+    sums = [0 for _ in range(n+1)]
+    for j in range(1, n + 1):
+        sums[j] = sums[j - 1] + int(string[j - 1])
 
     result = 0
     for size in range(2, n+1, 2):
-        for i in range(0, n-size+1):
-            if sums[i+int(size/2)] - sums[i] == sums[i+size] - sums[i+int(size/2)]:
+        for j in range(0, n - size + 1):
+            if sums[j + int(size / 2)] - sums[j] == sums[j + size] - sums[j + int(size / 2)]:
                 result = max(result, size)
 
     return result
@@ -18,5 +18,5 @@ def largest_sum_symmetry(str):
 if __name__ == '__main__':
     t = int(input())
     for i in range(t):
-        str = input()
-        print(largest_sum_symmetry(str))
+        s = input()
+        print(largest_sum_symmetry(s))
--- src/practices/practice/largest_cons_subseq/script.py
+++ src/practices/practice/largest_cons_subseq/script.py
@@ -1,8 +1,14 @@
 # https://practice.geeksforgeeks.org/problems/longest-consecutive-subsequence2449/1#
-
-#  approach is to keep a hash of the arr
-#  iterate over arr and if it is the first element of the subsequence, get size of the subsequence
-#  return max count
+#
+# Observations:
+#   - Note that there can be multiple subsets of different size which are consecutive in themselves
+#   - If smallest element of such subset is X, then X-1 would not exist in the array
+#   - If largest element of such subset is Y, then Y+1 would also not exist in the array
+#   - Using second point we can identify the smallest element of such subsets
+#   - After that we can keep finding the next element and keep count
+#   - We will stop if the next consecutive element is not present in the array
+#   - We can report the max size we found
+#
 
 import atexit
 import io
@@ -10,11 +16,11 @@ import sys
 
 
 class Solution:
-    def findLongestConseqSubseq(self, arr, N):
+    def findLongestConseqSubseq(self, arr, n_value):
         hashed = {val for val in arr}
 
         count = 0
-        for i in range(N):
+        for i in range(n_value):
             if not hashed.__contains__(arr[i] - 1):
                 val = arr[i]
                 temp = 0
