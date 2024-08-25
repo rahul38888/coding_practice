0ede11df970f6f81af612c53f5d22df0a16b97d3
rahul38888 <rahul38888@gmail.com>
2024-08-25 13:00:08 +0530

    Largest subarray with 0 sum

--- src/practices/practice/largest_contineous_zero_sum_subarray/script.py
+++ src/practices/practice/largest_contineous_zero_sum_subarray/script.py
@@ -1,20 +1,27 @@
 # https://practice.geeksforgeeks.org/problems/largest-subarray-with-0-sum/1#
-
-# Approach is to iterate over list and and store sum and index in hashed map if not present
-# if sum already present update max_len with (cur index - index present for the sum in map)
+#
+# Observations:
+#   - Let a subarray with sum zero is [x ... y]
+#       - If sum of subarray [0 ... x) is A then sum of subarray [0, y] is also A
+#   - This is something we can use
+#   - Start adding element in array one by one
+#   - If at any point the current sum has seen observed previously, then
+#       the subarray (previous sum index, current sum index] will be a 0 sum subarray
+#   - Find all such subarray and return max size
+#
 
 class Solution:
-    def maxLen(self, n, arr):
+    def maxLen(self, n_value: int, array: list):
         sum_hashed = dict()
         sum_hashed[0] = -1
         s = 0
         max_len = 0
-        for i in range(n):
-            s += arr[i]
+        for j in range(n_value):
+            s += array[j]
             if sum_hashed.__contains__(s):
-                max_len = max(max_len, i - sum_hashed[s])
+                max_len = max(max_len, j - sum_hashed[s])
             else:
-                sum_hashed[s] = i
+                sum_hashed[s] = j
 
         return max_len
 
