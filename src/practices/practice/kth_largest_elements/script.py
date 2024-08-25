20947bd83a357ded2e045f97a0736341defe5286
rahul38888 <rahul38888@gmail.com>
2024-08-25 08:50:02 +0530

    New changes

--- src/practices/practice/kth_largest_elements/script.py
+++ src/practices/practice/kth_largest_elements/script.py
@@ -1,5 +1,6 @@
 import heapq
 
+
 class Solution:
     """
         Problem: https://www.geeksforgeeks.org/batch/gts-1/track/GTS-ARRAY/problem/k-largest-elements4206
@@ -7,6 +8,11 @@ class Solution:
         Observations
         ------------
         Simplest solution can be to sort the array using a performant working algorithm and fetch the k largest elements
+        But this will take us O(n log(n)) time.
+
+        We can use
+
+
 
         Solution
         ---------
@@ -16,11 +22,11 @@ class Solution:
         From heap pop all elements one by one into an array. Return that array in reverse order
     """
 
-    def kLargest(self, arr, n, k):
+    def kLargest(self, array: list, count: int, k_count: int):
         heap = []
-        for a in arr:
+        for a in array:
             heapq.heappush(heap, a)
-            if len(heap) > k:
+            if len(heap) > k_count:
                 heapq.heappop(heap)
 
         result = []
